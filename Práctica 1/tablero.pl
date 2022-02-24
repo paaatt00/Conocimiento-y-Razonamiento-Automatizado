80403e0a0fd9dd19e76309e4f50f3e311e3d44f6
Atagan <Laura.RamosMartinez@edu.uah.es>
2022-02-24 17:57:36 +0100

    Update tablero.pl

--- Práctica 1/tablero.pl
+++ Práctica 1/tablero.pl
@@ -2,25 +2,30 @@
 
 % pintar un tablero pocho
 
+tablero_prueba([[1,2,3,4,5,6,7],
+                [0,0,0,0,0,0,0]]).
+
 escribir_lista([]).
-escribir_lista([X|Y]):- write(X),write(''), escribir_lista(Y). % escribir_lista([1,' ',2,' ',3,' ',4,' ',5,' ',6,' ',7])
+escribir_lista([X|Y]):- write(X),write(' '), escribir_lista(Y). % escribir_lista([1,' ',2,' ',3,' ',4,' ',5,' ',6,' ',7])
 
 escribir_lista_con_barra([]).
 escribir_lista_con_barra([X|Y]):- write(X), write('|'), escribir_lista_con_barra(Y). % escribir_lista_con_barra([1,' ',2,' ',3,' ',4,' ',5,' ',6,' ',7]).
 
-lista_repe(1,X,[X]).
-lista_repe(N,X,[X|L]):-
-                      N1 is N-1,
-                      lista_repe(N1,X,L).
+
+lista_repe(N, Out) :-
+   length(Out, N),
+   maplist(=(-), Out).
                       
 escribir_tablero([]):- nl.
 escribir_tablero([X|L]):- escribir_lista(X), nl,
-                          lista_repe(15,'-',L1), escribir_lista(L1) nl,
+                          lista_repe(15,L1), escribir_lista(L1), nl,
                           escribir_tablero(L).
-                          
-prueba1:- lista_repe(15,'-',L1), escribir_lista(L1).
+
+
+prueba1:- lista_repe(15,Y),escribir_lista(Y).
 
 prueba2:- escribir_lista([1,2,3,4,5,6,7]).
 
-prueba3:-
+prueba3:- tablero_prueba(Tp), escribir_tablero(Tp).
 
+s

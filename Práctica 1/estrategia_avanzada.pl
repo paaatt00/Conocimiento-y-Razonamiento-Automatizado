d2d0df690576dff258025da546409769bcac2c6a
Atagan <Laura.RamosMartinez@edu.uah.es>
2022-03-21 18:45:15 +0100

    El jugnado

--- Práctica 1/estrategia_avanzada.pl
+++ Práctica 1/estrategia_avanzada.pl
@@ -69,3 +69,46 @@ mejor_posicion(Tablero, Lista_columnas, Posicion):- mejor_posicion_horizontal(Ta
                                                         )
                                                     )
                                                     .
+
+jugando_maquina_avanzada(Tablero, Lista_columnas, Jugador):-  write('Comienza el turno del jugador '), write(Jugador), nl,
+                                            (
+                                                Jugador == 'x' -> leer_columna(Columna, Lista_columnas, Tablero)
+
+                                                ;
+
+                                                Jugador == 'o' -> mejor_posicion(Lista_columnas, Tablero, Columna)
+                                            ),
+                                          
+                                            extraer_columna(Columna, Tablero, Col),
+                                            introducir_ficha(Col, Jugador, Col2),
+                                            introducir_col(Col2, Tablero, Columna, Tablero2),
+
+                                            imprimir_mesa(Tablero2),
+                                            (
+                                                comprobar_victoria(Jugador, Tablero2) -> write('              ██╗░░░██╗██╗░█████╗░████████╗░█████╗░██████╗░██╗░█████╗░'), nl, 
+                                                                                         write('              ██║░░░██║██║██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗██║██╔══██╗'), nl,
+                                                                                         write('              ╚██╗░██╔╝██║██║░░╚═╝░░░██║░░░██║░░██║██████╔╝██║███████║'), nl,
+                                                                                         write('              ░╚████╔╝░██║██║░░██╗░░░██║░░░██║░░██║██╔══██╗██║██╔══██║'), nl,
+                                                                                         write('              ░░╚██╔╝░░██║╚█████╔╝░░░██║░░░╚█████╔╝██║░░██║██║██║░░██║'), nl,
+                                                                                         write('              ░░░╚═╝░░░╚═╝░╚════╝░░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚═╝'), nl, nl,
+                                                                                         write('         ▄  █ ██      ▄▄▄▄▄         ▄▀  ██      ▄   ██   ██▄   ████▄   ▄   ▄   ▄   ▄ '), nl,
+                                                                                         write('        █   █ █ █    █     ▀▄     ▄▀    █ █      █  █ █  █  █  █   █  █   █   █   █  '), nl,
+                                                                                         write('        ██▀▀█ █▄▄█ ▄  ▀▀▀▀▄       █ ▀▄  █▄▄█ ██   █ █▄▄█ █   █ █   █ █   █   █   █   '), nl,
+                                                                                         write('        █   █ █  █  ▀▄▄▄▄▀        █   █ █  █ █ █  █ █  █ █  █  ▀████ █   █   █   █   '), nl,
+                                                                                         write('           █     █                 ███     █ █  █ █    █ ███▀                        '), nl,
+                                                                                         write('          ▀     █                         █  █   ██   █              ▀   ▀   ▀   ▀   '), nl,
+                                                                                         write('               ▀                         ▀           ▀                               '), nl, nl,
+                                                                                         write('                              Ha ganado el jugador '), write(Jugador), nl, nl,
+                                                                                         write('                              EL PUBLICO ENLOQUECE!!!                                '), nl, nl,
+                                                                                         write('༼ ºل͟º ༼ ºل͟º ༼ ºل͟º ༽ ºل͟º ༽ ºل͟º ༽ ༼ ºل͟º ༼ ºل͟º ༼ ºل͟º ༽ ºل͟º ༽ ºل͟º ༽ ༼ ºل͟º ༼ ºل͟º ༼ ºل͟º ༽ ºل͟º ༽ ºل͟º ༽'), nl,
+                                                                                         write('༼ ºل͟º ༼ ºل͟º ༼ ºل͟º ༽ ºل͟º ༽ ºل͟º ༽ ༼ ºل͟º ༼ ºل͟º ༼ ºل͟º ༽ ºل͟º ༽ ºل͟º ༽ ༼ ºل͟º ༼ ºل͟º ༼ ºل͟º ༽ ºل͟º ༽ ºل͟º ༽'), nl,
+                                                                                         write('༼ ºل͟º ༼ ºل͟º ༼ ºل͟º ༽ ºل͟º ༽ ºل͟º ༽ ༼ ºل͟º ༼ ºل͟º ༼ ºل͟º ༽ ºل͟º ༽ ºل͟º ༽ ༼ ºل͟º ༼ ºل͟º ༼ ºل͟º ༽ ºل͟º ༽ ºل͟º ༽'), nl,
+                                                                                         write('༼ ºل͟º ༼ ºل͟º ༼ ºل͟º ༽ ºل͟º ༽ ºل͟º ༽ ༼ ºل͟º ༼ ºل͟º ༼ ºل͟º ༽ ºل͟º ༽ ºل͟º ༽ ༼ ºل͟º ༼ ºل͟º ༼ ºل͟º ༽ ºل͟º ༽ ºل͟º ༽'), nl, nl
+                                            ;
+                                                (
+                                                    Jugador == 'x' -> Jugador2 = 'o'
+                                                ;
+                                                    Jugador == 'o' -> Jugador2 = 'x'
+                                                ),
+                                                jugando_maquina_avanzada(Tablero2, Lista_columnas, Jugador2)
+                                            ).

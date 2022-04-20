83d63e84d618ed82367f6f82e7317e93cf65b6fb
Atagan <Laura.RamosMartinez@edu.uah.es>
2022-04-20 16:58:26 +0200

    Creo que hay que tocar parte grande de la gramatica para que estén bien los generos y numeros, mañana lo hablamos

--- Práctica 2/gramatica.pl
+++ Práctica 2/gramatica.pl
@@ -40,37 +40,37 @@ oracion_coordinada(oc(Oracion1, Nexo1, Oracion2, Nexo2, Oracion3)) --> oracion_a
 % SINTAGMA NOMINAL (gn)
 
 grupo_nominal(GN) --> grupo_nominal_aux(GN).
-grupo_nominal(gn(Det, GN)) --> determinante(Det), grupo_nominal_aux(GN).
+grupo_nominal(gn(Det, GN)) --> determinante(Det, G, N), grupo_nominal_aux(GN).
 grupo_nominal(gn(GN1, Nexo, GN2)) --> grupo_nominal_aux(GN1), nexo(Nexo), grupo_nominal_aux(GN2).
 grupo_nominal(gn(GN, GPrep)) --> grupo_nominal_aux(GN), grupo_preposicional(GPrep).
 grupo_nominal(gn(GN, GAadj)) --> grupo_nominal_aux(GN), grupo_adjetival(GAadj).
 grupo_nominal(gn(GN, OrSub)) --> grupo_nominal_aux(GN), oracion_subordinada(OrSub).
 
-grupo_nominal_aux(gn(N)) --> nombre(N).
-grupo_nominal_aux(gn(N)) --> pronombre(N).
-grupo_nominal_aux(gn(NP)) --> nombre_propio(NP).
-grupo_nominal_aux(gn(Det, N)) --> determinante(Det), nombre(N).
-grupo_nominal_aux(gn(N, CN)) --> nombre(N), grupo_adjetival(CN).
-grupo_nominal_aux(gn(CN, N)) --> grupo_adjetival(CN), nombre(N).
-grupo_nominal_aux(gn(Det, CN, N)) --> determinante(Det), grupo_adjetival(CN), nombre(N).
-grupo_nominal_aux(gn(Det, N, CN)) --> determinante(Det), nombre(N), grupo_adjetival(CN).
-grupo_nominal_aux(gn(Det, N, CN)) --> determinante(Det), nombre(N), grupo_preposicional(CN).
+grupo_nominal_aux(gn(Nom)) --> nombre(Nom, _, _).
+grupo_nominal_aux(gn(Nom)) --> pronombre(Nom, _, _).
+grupo_nominal_aux(gn(NP)) --> nombre_propio(NP, _, _).
+grupo_nominal_aux(gn(Det, Nom)) --> determinante(Det, G, N), nombre(Nom, G, N).
+grupo_nominal_aux(gn(N, CN)) --> nombre(N, _, _), grupo_adjetival(CN).
+grupo_nominal_aux(gn(CN, Nom)) --> grupo_adjetival(CN), nombre(Nom, _, _).
+grupo_nominal_aux(gn(Det, CN, Nom)) --> determinante(Det, G, N), grupo_adjetival(CN), nombre(Nom, G, N).
+grupo_nominal_aux(gn(Det, Nom, CN)) --> determinante(Det, G, N), nombre(Nom, G, N), grupo_adjetival(CN).
+grupo_nominal_aux(gn(Det, Nom, CN)) --> determinante(Det, G, N), nombre(Nom, G, N), grupo_preposicional(CN).
 
 % SINTAGMA VERBAL (gv)
 
-grupo_verbal(gv(V)) --> verbo(V).
-grupo_verbal(gv(V, GV)) --> verbo_copulativo(V), atributo(GV).
-grupo_verbal(gv(V, GV)) --> verbo(V), grupo_nominal(GV).
-grupo_verbal(gv(V, GV1, GV2)) --> verbo(V), grupo_nominal(GV1), grupo_preposicional(GV2).
-grupo_verbal(gv(V, GV1, GV2)) --> verbo(V), grupo_nominal(GV1), grupo_adjetival(GV2).
-grupo_verbal(gv(V, GV)) --> verbo_copulativo(V), grupo_adverbial(GV).
-grupo_verbal(gv(V, GV)) --> verbo(V), grupo_adjetival(GV).
-grupo_verbal(gv(V, GV)) --> verbo(V), grupo_preposicional(GV).
-grupo_verbal(gv(V, GV)) --> verbo(V), grupo_adverbial(GV).
-grupo_verbal(gv(V, GV1, GV2)) --> verbo(V), grupo_adverbial(GV1), grupo_nominal(GV2).
-grupo_verbal(gv(V, GV1, GV2)) --> verbo(V), grupo_preposicional(GV1), grupo_nominal(GV2).
-grupo_verbal(gv(GV1, V, GV2)) --> grupo_adverbial(GV1), verbo(V), grupo_preposicional(GV2).
-grupo_verbal(gv(V, OrSub)) --> verbo_copulativo(V), oracion_subordinada(OrSub).
+grupo_verbal(gv(V)) --> verbo(V, _).
+grupo_verbal(gv(V, GV)) --> verbo_copulativo(V, _), atributo(GV).
+grupo_verbal(gv(V, GV)) --> verbo(V, _), grupo_nominal(GV).
+grupo_verbal(gv(V, GV1, GV2)) --> verbo(V, _), grupo_nominal(GV1), grupo_preposicional(GV2).
+grupo_verbal(gv(V, GV1, GV2)) --> verbo(V, _), grupo_nominal(GV1), grupo_adjetival(GV2).
+grupo_verbal(gv(V, GV)) --> verbo_copulativo(V, _), grupo_adverbial(GV).
+grupo_verbal(gv(V, GV)) --> verbo(V, _), grupo_adjetival(GV).
+grupo_verbal(gv(V, GV)) --> verbo(V, _), grupo_preposicional(GV).
+grupo_verbal(gv(V, GV)) --> verbo(V, _), grupo_adverbial(GV).
+grupo_verbal(gv(V, GV1, GV2)) --> verbo(V, _), grupo_adverbial(GV1), grupo_nominal(GV2).
+grupo_verbal(gv(V, GV1, GV2)) --> verbo(V, _), grupo_preposicional(GV1), grupo_nominal(GV2).
+grupo_verbal(gv(GV1, V, GV2)) --> grupo_adverbial(GV1), verbo(V, _), grupo_preposicional(GV2).
+grupo_verbal(gv(V, OrSub)) --> verbo_copulativo(V, _), oracion_subordinada(OrSub).
 
 % SINTAGMA PREPOSICIONAL (gp)
 
@@ -78,8 +78,8 @@ grupo_preposicional(gp(Enlace, Termino)) --> preposicion(Enlace), grupo_nominal(
 
 % SINTAGMA ADJETIVAL (gadj)
 
-grupo_adjetival(gadj(Adj)) --> adjetivo(Adj).
-grupo_adjetival(gadj(Adj, CAdj)) --> adjetivo(Adj), grupo_preposicional(CAdj).
+grupo_adjetival(gadj(Adj)) --> adjetivo(Adj, _, _).
+grupo_adjetival(gadj(Adj, CAdj)) --> adjetivo(Adj, _, _), grupo_preposicional(CAdj).
 
 % SINTAGMA ADVERBIAL (gadv)
 
